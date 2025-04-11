53cb103e8b4fa3da7c609904ec568a63357794e7
MattsVini <151637752+MattsVini@users.noreply.github.com>
2025-04-11 15:20:00 -0300

    Movimento do Cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("*** movimento das peças de xadrez ***\n");
-    int bispoMover = 1, torreMover = 1, rainhaMover = 1;
+    int bispoMover = 1, torreMover = 1, rainhaMover = 1, cavaloMover = 1;
     printf("Movimento da Torre:\n");
     while (torreMover < 6){
         printf("Direita(x%d)\n", torreMover);
@@ -19,6 +19,18 @@ int main() {
     printf("\n\nMovimento da Rainha\n");
     for(rainhaMover; rainhaMover < 9; rainhaMover++){
         printf("Direita(x%d)\n", rainhaMover);
+    }
+
+    printf("\n\nMovimento do Cavalo\n");
+
+    for(cavaloMover; cavaloMover < 2; cavaloMover++){
+        int i = 1;
+        while(i < 3){
+            printf("Baixo(x%d)\n", i);
+            i++;
+        }
+        printf("Esquerda\n");
+        
     }
         return 0;
 }
